4256fc3c16b36c74edc5e02031f35fdd961764c4
Kylorcache <kylorcache@gmail.com>
2015-01-24 17:46:53 -0600

    Austin; Biggest Storyboard Changes Ever (4.6)

---
+++ RecycleRush/MatchAnalysis/AnalysisTeamPageViewController.h
@@ -0,0 +1,15 @@
+//
+//  AnalysisTeamPageViewController.h
+//  RecycleRush
+//
+//  Created by FRC on 1/24/15.
+//  Copyright (c) 2015 FRC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class DataManager;
+
+@interface AnalysisTeamPageViewController : UIViewController
+@property (nonatomic, strong) DataManager *dataManager;
+
+@end
