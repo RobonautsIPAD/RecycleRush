9b22810bec318bd738aafd860c65a666783c8dfd
mtsprite <castiel.salazar@yahoo.com>
2015-02-15 15:41:47 -0600

    .

--- RecycleRush/TeamScouting/pitScoutingDataSheet.h
+++ RecycleRush/TeamScouting/pitScoutingDataSheet.h
@@ -8,8 +8,7 @@
 
 #import <UIKit/UIKit.h>
 @class DataManager;
-@class TeamData;
-@interface PitScoutingDataSheet : UIViewController <UITextFieldDelegate>
+@interface PitScoutingDataSheet : UIViewController <UIActionSheetDelegate, UITextFieldDelegate>
 
 @property (nonatomic, strong) DataManager *dataManager;
 @property (nonatomic, strong) TeamData *team;
